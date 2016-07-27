5e1199a30e38f2bf9863be2a284fc2d49deb52ee
david-salgado <david.salgado.fernandez@gmail.com>
2016-07-27 18:15:57 +0200

    Update ReadSASFile

--- R/ReadSASFile.R
+++ R/ReadSASFile.R
@@ -28,7 +28,7 @@
 #' \dontrun{
 #' # We assume that the SAS file \code{MM032014.sas7bdat} is in the 
 #' #administrator desktop:
-#' SASName <- 'C:/Users/Administrador/Desktop/MM032014.sas7bdat'
+#' SASName <- 'C:/Users/David/Desktop/MM032014.sas7bdat'
 #' # We assume data created previosly:
 #' data(ExampleDD)
 #' Example.DM <- ReadSASFile(SASName, ExampleDD)
@@ -45,7 +45,6 @@
 #' @export
 ReadSASFile <- function(SASFileName, DD){
     
-    VNC <- Reduce('+', getVNC(DD))
     out.SP <- haven::read_sas(SASFileName)
     ColClasses <- unlist(lapply(out.SP, class))
     setDT(out.SP)
@@ -56,71 +55,26 @@ ReadSASFile <- function(SASFileName, DD){
         
     }
     
-
+    VNC <- getVNC(DD)
     CalID <- getIDQual(VNC)
-    CalID <- intersect(names(VNC), CalID)
-  
-    for (cal in CalID) {
-        
-        VNC[get(cal) == '.', cal := '', with = F]
-        
-    }
-    
-
     CalNoID <- getNonIDQual(VNC)
-    CalNoID <- intersect(names(VNC), CalNoID)
+    Cals <- unique(union(CalID, CalNoID))
     
-    
-    Cals <- union(CalID, CalNoID)
-    VarSP <- VNC$Unit1
-    VarSP <- VarSP[!is.na(VarSP) & VarSP != ""]
-    MissVar <- setdiff(names(out.SP), VarSP)
-    
-    if (length(MissVar) > 0) stop(paste0('[RepoReadWrite::ReadSASFile] The following variables in the SAS file are not present in the Excel sheet:\n\n', 
-                                        paste0(MissVar, collapse = ' '), '\n\n'))
-    #VarSP <- intersect(VarSP, names(out.SP))
-    #out.SP <- out.SP[, VarSP, with = F]
-    
-    VNC <- VNC[IDDD == "" & IDQual != "", IDDD := IDQual]
-    VNC <- VNC[IDDD == "" & NonIDQual != "", IDDD := NonIDQual]
-    
-    pasteNA <- function(x, y){
-        out <- ifelse(is.na(y) | y == '', paste0(x, ''), paste(x, y, sep = "_"))
-        return(out)
-    }
-    
-    VNC <- copy(VNC)[, NewVar := IDDD]
-    VNC <- VNC[IDQual != '' & IDDD == '' & Unit1 != '', NewVar := IDQual]
-    VNC <- VNC[NonIDQual != '' & IDDD == '' & Unit1 != '', NewVar := NonIDQual]
-    
-    for (Cal in Cals) {
-        VNC <- copy(VNC)[, NewVar := pasteNA(NewVar, get(Cal))]
-    }
-    
-    EquivalName <- names(out.SP)
-    names(EquivalName) <- unlist(lapply(EquivalName, function(x) {VNC[VNC[['Unit1']] == x, NewVar]}))
-    setnames(out.SP, EquivalName, names(EquivalName))
-    
-
-    slots <- setdiff(slotNames(DD), 'VarNameCorresp')
-    DDdt <- new(Class = 'DDdt')
-    for (slot in slots) {DDdt <- DDdt + slot(DD, slot) }
-    
-    DDVarNames <- unlist(lapply(as.list(names(out.SP)), ExtractNames))
-    NoDDdt <- setdiff(DDVarNames, DDdt[['Variable']])
-    if (length(NoDDdt) > 0) {stop('[RepoReadWrite::ReadSASFile] The following variables in the SAS file are not present in the DD file:\n\n', 
-                                  paste0(NoDDdt, collapse = ' '), '\n\n')
-    
-    } else {
+    VNC <- lapply(VNC, function(VNCdt){
+        
+        for (cal in intersect(names(VNCdt), Cals)) {
             
-        names(DDVarNames) <- names(out.SP)
+            VNCdt[get(cal) == '.', cal := '', with = F]
+            
+        }    
+        return(VNCdt)
+    })
     
-    }
-
-    for (Var in names(out.SP)) {
-
-            out.SP[, Var := as(get(Var), DDdt[Variable == DDVarNames[[Var]], Class]), with = F]
-    }
-    return(out.SP)
+    VNC <- new(Class = 'VarNameCorresp', VNC)
     
+    IDDDNames <- UnitToIDDDNames(VNC, names(out.SP))
+    
+    setnames(out.SP, IDDDNames[['UnitName']], IDDDNames[['IDDDName']])
+    
+    return(out.SP) 
 }
