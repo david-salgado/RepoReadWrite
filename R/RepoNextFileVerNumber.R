33aa692e619d02e0cc0cef1e3e7c2557ce621afd
elisa-esteban <elisa.esteban.segurado@gmail.com>
2019-10-18 11:17:18 +0200

    Update RepoNextFileVerNumber with the type of file versio (D or P)

--- R/RepoNextFileVerNumber.R
+++ R/RepoNextFileVerNumber.R
@@ -16,6 +16,8 @@
 #' @param Base Character vector of length 1 with the year ('aaaa') of the base to which data are
 #' referred. If it has no sense, "Base" is a empty character vector.
 #'  
+#' @param DorP Character vector with the type of version (D or P) of the files.
+#' 
 #' @return It returns a character vector of length equal to the length of \code{Periods} with the 
 #' next version of each input file name.
 #'
@@ -25,7 +27,7 @@
 #' }
 #' 
 #' @export
-RepoNextFileVerNumber <- function(Periods, Path, FileType, Base){
+RepoNextFileVerNumber <- function(Periods, Path, FileType, Base, DorP = 'D'){
   
   Periods.RepoTime <- try(newRepoTime(Periods))
   if (inherits(Periods.RepoTime, 'try-error')) stop('[RepoReadWrite::RepoNextFileVerNumber] The time period does not have a valid format. Please, introduce a valid period.\n\n')
@@ -36,81 +38,55 @@ RepoNextFileVerNumber <- function(Periods, Path, FileType, Base){
   
   if (!FileType %in% c('FF', 'FD', 'FG', 'FA', 'FI', 'FP', 'FT', 'FL')) stop('[RepoReadWrite::RepoNextFileVerNumber] The allowed file types are FF, FD, FG, FA, FI, FP, FT, FL.\n')
       
+  if (DorP != 'D' & DorP != 'P') stop('[RepoReadWrite::RepoNextFileVerNumber] DorP must be D or P.\n')
+  
   Files <- list.files(Path)
   Files <- Files[grep(FileType, Files)]
   if (Base != '') Files <- Files[grep(paste0('B', substr(Base, 3, 4)), Files)]
 
   if (length(Files) == 0) {
       
+      if (Base != '') warning(paste0('[RepoReadWrite::RepoNextFileVerNumber] Check if Base should be blank or not.'))
+          
       matchFF <- pmatch('FF', FileType)
+      if (!is.na(matchFF) & matchFF == 1) NextVer <- rep(paste0('.', DorP, '_1'), length(Periods))
       matchFA <- pmatch('FA', FileType)
-      matchFX <- sum(c(matchFF, matchFA), na.rm = TRUE)
-      if (!is.na(matchFX) & matchFX == 1) NextVer <- rep('.D_1', length(Periods))
-      
+      if (!is.na(matchFA) & matchFA == 1) NextVer <- rep(paste0('.', DorP, '_1'), length(Periods))
       matchFI <- pmatch('FI', FileType)
+      if (!is.na(matchFI) & matchFI == 1) NextVer <- rep(paste0('.', DorP, '_1'), length(Periods))
       matchFP <- pmatch('FP', FileType)
+      if (!is.na(matchFP) & matchFP == 1) NextVer <- rep(paste0('.', DorP, '_1'), length(Periods))
       matchFD <- pmatch('FD', FileType)
+      if (!is.na(matchFD) & matchFD == 1) NextVer <- rep(paste0('.', DorP, '_1'), length(Periods))
       matchFG <- pmatch('FG', FileType)
-      matchFY <- sum(c(matchFI, matchFP, matchFD, matchFG), na.rm = TRUE)
-      if (!is.na(matchFY) & matchFY == 1) NextVer <- rep('.P_1', length(Periods))
+      if (!is.na(matchFG) & matchFG == 1) NextVer <- rep(paste0('.', DorP, '_1'), length(Periods))
+      
       names(NextVer) <- Periods
       
       return(NextVer)
   }
   
-  SelFiles <- c()
+  nextVersions <- vector(mode = 'integer', length = length(Periods))
+  names(nextVersions) <- Periods
   for (Per in Periods) {
     
-    aux <- strsplit(Files[grep(Per, Files)], Per)
-    aux <- lapply(aux, function(x){
-        
-      x[1] <- paste0(x[1], Per)
-      return(x)
+    perFiles <- Files[grep(paste0(Per, '.[', DorP, ']_'), Files)]
     
-    })
-    
-    if (length(aux) == 0) {
+    if (length(perFiles) == 0) nextVersions[Per] <- 1
+    if (length(perFiles) != 0) {
       
-        TypesX <- c('FF', 'FA')
-        Flag.TypeX <- lapply(TypesX, function(Type){
-          
-          length(FileType[grep(Type, FileType)])
-        })
-        Flag.TypeX <- sum(unlist(Flag.TypeX))
-        if (Flag.TypeX > 0) aux <- matrix(c(Per, '.D_0'), ncol = 2)
+      nextVersions[Per] <- max(sapply(perFiles, function(fileName){
+
+        as.integer(strsplit(fileName, split = paste0(Per, '.[', DorP, ']_'))[[1]][2]) + 1
       
-        TypesY <- c('FI', 'FP', 'FD', 'FG')
-        Flag.TypeY <- lapply(TypesY, function(Type){
-          
-                    length(FileType[grep(Type, FileType)])
-                  })
-        Flag.TypeY <- sum(unlist(Flag.TypeY))
-        if (Flag.TypeY > 0) aux <- matrix(c(Per, '.P_0'), ncol = 2)
-        
-    } else {
-      
-      aux <- Reduce(rbind, aux, init = aux[[1]])
+      }))
       
     }
-      
-    SelFiles <- rbind(SelFiles, aux)
   }
-
-  OrderedPeriods <- unique(SelFiles[, 1])
-  rownames(SelFiles) <- NULL
-  SelFiles <- as.data.frame(SelFiles)
-  SelFiles <- split(SelFiles, SelFiles[, 1])[OrderedPeriods]
-  NextVer <- lapply(SelFiles, function(df){
-      
-      nVer <- unlist(strsplit(as.character(df[nrow(df), 2]), '_'))
-      nVer[2] <- as.integer(nVer[2]) + 1
-      nVer <- paste0(nVer[1], '_', nVer[2])
-      return(nVer)
-  })
   
-  names(NextVer) <- Periods
-  NextVer <- unlist(NextVer)
+  nextVersions <- paste0('.', DorP, '_', nextVersions)
+  names(nextVersions) <- Periods
   
-  return(NextVer)
+  return(nextVersions)
   
 }
